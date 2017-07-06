fa69a591363b7f36c06724d7b799c69d2600d3ca
mynawang <18267853511@163.com>
2017-07-06 17:11:15 +0800

    调整代码

--- Lucene-HelloWorld/src/main/java/com/mynawang/lucene/HelloWorld.java
+++ Lucene-HelloWorld/src/main/java/com/mynawang/lucene/HelloWorld.java
@@ -16,11 +16,10 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.store.FSDirectory;
 
+import java.io.File;
 import java.io.IOException;
-
-import static junit.framework.Assert.assertEquals;
 
 /**
  * @auther mynawang
@@ -32,9 +31,10 @@ public class HelloWorld {
         // 实例化词法分析器
         Analyzer analyzer = new StandardAnalyzer();
         // 确定索引文件位置（内存中）
-        Directory directory = new RAMDirectory();
+        // Directory directory = new RAMDirectory();
+
         // 确定文件在硬盘中，用下面这段
-        //Directory directory = FSDirectory.open("/tmp/testindex");
+        Directory directory = FSDirectory.open(new File("G:\\Rabbish\\testindex").toPath());
         // 对indexWriter进行配置
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         IndexWriter iwriter = new IndexWriter(directory, config);
@@ -42,7 +42,7 @@ public class HelloWorld {
         Document doc = new Document();
         // 将要用来索引的句子
         String text = "This is the text to be indexed.";
-        // 存储为fieldname文件
+        // 存储text,表名为fieldname
         doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
         // doc对象添加到索引创建中
         iwriter.addDocument(doc);
@@ -53,21 +53,22 @@ public class HelloWorld {
         DirectoryReader ireader = DirectoryReader.open(directory);
         // 创建索引搜索
         IndexSearcher isearcher = new IndexSearcher(ireader);
+        // 创建查询器，查询为fieldname的表
         // Parse a simple query that searches for "text":
         QueryParser parser = new QueryParser("fieldname", analyzer);
+        // 查询包含“text”
         Query query = parser.parse("text");
-        ScoreDoc[] hits = isearcher.search(query, 5).scoreDocs;
-        System.out.println(JSON.toJSON(hits));
-
-        System.out.println(1 == hits.length);
-        // Iterate through the results:
+        // 查询得到的结果集
+        ScoreDoc[] hits = isearcher.search(query, 100).scoreDocs;
+        System.out.println(JSON.toJSONString(hits));
+        // 遍历结果
         for (int i = 0; i < hits.length; i++) {
+            System.out.println(i);
             Document hitDoc = isearcher.doc(hits[i].doc);
-            assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
+            System.err.println("This is the text to be indexed.".equals(hitDoc.get("fieldname")));
         }
         ireader.close();
         directory.close();
-
     }
 
 
